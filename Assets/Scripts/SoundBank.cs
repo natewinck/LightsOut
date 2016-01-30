17efc610027ea5d6596b132ed2dc853afabaa30d
Neocheese <mvaughnii0001@gmail.com>
2016-01-30 12:58:35 -0500

    Added SoundBank script

---
+++ Assets/Scripts/SoundBank.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SoundBank : MonoBehaviour
+{
+  public List<AudioClip> SoundClips;
+
+  private ShuffleDeck m_SoundDeck;
+
+  // Use this for initialization
+  void Start () {
+    m_SoundDeck = new ShuffleDeck(SoundClips);
+  }
+	AudioClip Draw () {
+		return (AudioClip) m_SoundDeck.Draw();
+	} 
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+}
+
