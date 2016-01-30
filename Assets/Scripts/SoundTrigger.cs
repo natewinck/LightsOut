0e48073acbd678e5915ecdac4d92d9d4242c727e
mildmojo <theaterfire@yahoo.com>
2016-01-29 22:39:02 -0500

    Adds placeholder kitchen audio, SoundTrigger script.

--- Assets/Scripts/SoundTrigger.cs
+++ Assets/Scripts/SoundTrigger.cs
@@ -3,14 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
+[RequireComponent (typeof(AudioSource))]
 public class SoundTrigger : MonoBehaviour {
   public List<AudioClip> SoundBank;
 
   private ShuffleDeck soundDeck;
+  private AudioSource source;
 
   // Use this for initialization
   void Start () {
     soundDeck = new ShuffleDeck(SoundBank);
+    source = GetComponent<AudioSource>();
   }
 
   // Update is called once per frame
@@ -19,5 +22,9 @@ public class SoundTrigger : MonoBehaviour {
   }
 
   void OnTriggerEnter(Collider c) {
+    if (c.CompareTag("Player")) {
+      var clip = (AudioClip) soundDeck.Draw();
+      source.PlayOneShot(clip);
+    }
   }
 }
