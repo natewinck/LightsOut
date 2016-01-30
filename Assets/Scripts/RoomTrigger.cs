5021b3f7594aecb5122c78646a3deaa2af08479b
agtcap1 <agtcap1@yahoo.com>
2016-01-30 17:32:15 -0500

    Added audio transition

---
+++ Assets/Scripts/RoomTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class RoomTrigger : MonoBehaviour {
+    public AudioMixerSnapshot inside;
+    public AudioMixerSnapshot outside;
+    
+    void OnTriggerEnter (Collider c)
+    {
+        inside.TransitionTo(.25f);
+
+    }
+    void OnTriggerExit(Collider c)
+    {
+        outside.TransitionTo(.25f);
+
+    }
+}
