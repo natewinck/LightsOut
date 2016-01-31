117fc0f6c86a580782e50d0a592dbdeb99863b4d
Nathaniel Winckler <natewinck@gmail.com>
2016-01-31 17:49:26 -0500

    Initial working copy of the android scene

--- Assets/Scripts/PhoneButtonController.cs
+++ Assets/Scripts/PhoneButtonController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PhoneButtonController : MonoBehaviour {
 
   public UnityStandardAssets.Characters.FirstPerson.TankPersonController tankPersonController;
+  public PhoneOrientation phoneOrientation;
 
 	// Use this for initialization
 	void Start () {
@@ -13,11 +14,15 @@ public class PhoneButtonController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
     if (Input.touchCount > 0) {
-      // Get movement of the finger since last frame
-      //Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-      tankPersonController.PushForward();
-      // Move object across XY plane
-      //transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+      if(Input.GetTouch (0).position.x / Screen.width > 0.9f) {
+        // Get movement of the finger since last frame
+        //Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+        phoneOrientation.ResetOrientation();
+        // Move object across XY plane
+        //transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0)
+      } else {
+        tankPersonController.PushForward();
+      }
     } else {
       tankPersonController.StopPushForward();
     }
