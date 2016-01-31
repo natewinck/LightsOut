1fc44bda4f7a07fc8a2e7960e40753b1fa9e079b
mildmojo <theaterfire@yahoo.com>
2016-01-31 15:22:38 -0500

    Adds starting room and script to play audio & go to next level.

---
+++ Assets/Scripts/PlayAndChangeState.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent (typeof(AudioSource))]
+public class PlayAndChangeState : MonoBehaviour {
+  public List<AudioClip> clipList;
+  public string thenTransitionTo;
+
+  IEnumerator Start () {
+    var audio = GetComponent<AudioSource>();
+
+    // Play all the clips in sequence
+    foreach (var clip in clipList) {
+      audio.PlayOneShot(clip);
+      yield return new WaitForSeconds(clip.length + 0.5f);
+    }
+
+    // Change game state (usually "nextlevel")
+    GameManager.instance.gameState.TransitionTo(thenTransitionTo);
+  }
+
+  void Update () {
+
+  }
+}
