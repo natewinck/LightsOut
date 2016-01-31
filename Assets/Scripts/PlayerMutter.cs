712c1e47669b22884fed602c088ecb80758464c1
Nathaniel Winckler <natewinck@gmail.com>
2016-01-31 00:23:04 -0500

    Player mutter update to use Ontriggerenter

--- Assets/Scripts/PlayerMutter.cs
+++ Assets/Scripts/PlayerMutter.cs
@@ -50,6 +50,7 @@ public class PlayerMutter : MonoBehaviour {
   }
 
   void OnTriggerEnter(Collider other) {
+    Debug.Log (other.name);
     if (other.CompareTag("MutterBox") && other.GetComponent<SoundBank>() != null && !m_MouthAudioSource.isPlaying) {
       Debug.Log ("I'm collisioning with Mutter Box");
       // So we don't keep changing the audio clips when we're on the same floor,
